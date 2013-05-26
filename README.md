e20e6669c43d639f2cb5ae0a31c37486820303ba
Ryosuke IWANAGA <riywo.jp@gmail.com>
2013-05-26 02:18:17 -0700

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Net::AnimeNewsNetwork::Encyclopedia - It's new $module
+
+# SYNOPSIS
+
+    use Net::AnimeNewsNetwork::Encyclopedia;
+
+# DESCRIPTION
+
+Net::AnimeNewsNetwork::Encyclopedia is ...
+
+# LICENSE
+
+Copyright (C) Ryosuke IWANAGA.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Ryosuke IWANAGA <riywo.jp@gmail.com>
---
+++ lib/Net/AnimeNewsNetwork/Encyclopedia.pm
@@ -0,0 +1,39 @@
+package Net::AnimeNewsNetwork::Encyclopedia;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Net::AnimeNewsNetwork::Encyclopedia - It's new $module
+
+=head1 SYNOPSIS
+
+    use Net::AnimeNewsNetwork::Encyclopedia;
+
+=head1 DESCRIPTION
+
+Net::AnimeNewsNetwork::Encyclopedia is ...
+
+=head1 LICENSE
+
+Copyright (C) Ryosuke IWANAGA.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Ryosuke IWANAGA E<lt>riywo.jp@gmail.comE<gt>
+
+=cut
+
